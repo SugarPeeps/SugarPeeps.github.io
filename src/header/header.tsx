cff28d95877ac0e44dd829ed54de3db3d79ab8ff
Tim Heuett <tim.heuett@gmail.com>
2024-10-22 00:01:20 -0700

    WIP styling

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -7,26 +7,30 @@ import styles from './header.module.scss';
 
 export default function Header() {
     return (
-        <header>
+        <div>
             <div className={styles.topBar}>
-                <a href="/appointments">Book Now</a>
+                <a href="/appointments" className={styles.bookNowButton}>Book Now</a>
             </div>
-            <a href="/">
-                <Image src="/images/pinup.png" alt="Sugar Peeps logo" width={100} height={100} />
-            </a>
-            <div className="location">
-                <div className="address">{ContactAddress}</div>
-                <p>{ContactAddress2}</p>
+            <div className={styles.header}>
+                <a href="/">
+                    <Image src="/images/pinup.png" alt="Sugar Peeps logo" width={240} height={126} />
+                </a>
+                <div className="location">
+                    <div className="address">{ContactAddress}</div>
+                    <p>{ContactAddress2}</p>
+                </div>
+                <div className="contact">
+                    <div className="phone">{ContactPhone}</div>
+                    <p>Appointment Only</p>
+                </div>
+                <ul className="social">
+                    <li><a href={Instagram} target="_blank">Instagram</a></li>
+                    <li><a href={Facebook} target="_blank">Facebook</a></li>
+                </ul>
             </div>
-            <div className="contact">
-                <div className="phone">{ContactPhone}</div>
-                <p>Appointment Only</p>
+            <div className={styles.navBar}>
+                <Nav />
             </div>
-            <ul className="social">
-                <li><a href={Instagram} target="_blank">Instagram</a></li>
-                <li><a href={Facebook} target="_blank">Facebook</a></li>
-            </ul>
-            <Nav />
-        </header>
+        </div>
     )
 }
