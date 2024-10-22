f49baf3b7ddb6791b2c2af450b6c4854a5c10121
Tim Heuett <tim.heuett@gmail.com>
2024-10-22 02:55:59 -0700

    aria improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,10 @@ export default function Page() {
     return (
         <div>
         <Image src="/images/mspeeps2.jpg" alt="Ms Peeps" width={500} height={500} />
-        <h2>Hello, hellooo!</h2>
+        <h2 className="text-xl">Hello, hellooo!</h2>
         <p>We are so glad you're here.</p>
-        <p>Sugar Peeps is a space that welcomes and celebrates <b>EVERY BODY</b>. Genevieve believes strongly that sugaring and all personal services should be affirming and positive experiences. She strives to be a master of her chosen esthetic specialties, and offers a wealth of information regarding her thoughtfully curated offerings. From the beginning it has been Sugar Peeps mission to provide ethical, quality, caring services with a focus on natural, eco-friendly products. When you become a Sugar Peeps' peep rest assured you will be in skilled, caring, attentive hands.</p>
-        <p>Scheduling is easy! Simply click the Book Now button above and select your desired sugar services. You'll be supporting a small woman owned business, our local economy, AND choosing a natural hair removal method that is sweet to the environment and your skin. Bonus: Dottie Grace, Genevieve's gorgeous little shop pup familiar happily comes to work every day as well and is always available for emotional puppy support. <b><i>Welcome to the sweet side of self care!</i></b></p>
+        <p>Sugar Peeps is a space that welcomes and celebrates <strong>EVERY BODY</strong>. Genevieve believes strongly that sugaring and all personal services should be affirming and positive experiences. She strives to be a master of her chosen esthetic specialties, and offers a wealth of information regarding her thoughtfully curated offerings. From the beginning it has been Sugar Peeps mission to provide ethical, quality, caring services with a focus on natural, eco-friendly products. When you become a Sugar Peeps' peep rest assured you will be in skilled, caring, attentive hands.</p>
+        <p>Scheduling is easy! Simply click the Book Now button above and select your desired sugar services. You'll be supporting a small woman owned business, our local economy, AND choosing a natural hair removal method that is sweet to the environment and your skin. Bonus: Dottie Grace, Genevieve's gorgeous little shop pup familiar happily comes to work every day as well and is always available for emotional puppy support. <em>Welcome to the sweet side of self care!</em></p>
         {/* TODO: instagram embed */}
         </div>
     )
