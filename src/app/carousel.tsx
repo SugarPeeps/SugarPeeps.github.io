c3ca61b117242cdf24836816189f87fcd392f21d
Tim Heuett <tim.heuett@gmail.com>
2024-10-23 02:06:39 -0700

    add carousel timer; tweak styling

--- src/app/carousel.tsx
+++ src/app/carousel.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ChevronLeft, ChevronRight } from 'react-feather';
 
 export type Slide = {
@@ -11,9 +11,10 @@ export type Slide = {
 
 export type CarouselProps = {
     slides: Slide[];
+    intervalMs?: number;
 }
 
-export function Carousel({ slides }: CarouselProps) {
+export function Carousel({ slides, intervalMs = 5000 }: CarouselProps) {
     const [index, setIndex] = useState(0)
 
     const next = () => {
@@ -24,20 +25,25 @@ export function Carousel({ slides }: CarouselProps) {
         setIndex((index + 4) % 5)
     }
 
+    useEffect(() => {
+        const interval = setInterval(next, intervalMs)
+        return () => clearInterval(interval)
+    })
+
     return (
         <div className="relative aspect-video overflow-hidden">
             <div>
                 {slides && slides.map((slide, i) => (
                     <img key={i} src={slide.src} alt={slide.alt} 
-                        className={`absolute h-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${i == index ? "opacity-100" : "opacity-0"}`}/>
+                        className={`absolute h-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${i == index ? "opacity-100" : "opacity-0"} transition-opacity ease-in-out duration-500`}/>
                 ))}
             </div>
 
             <div className="absolute inset-0 flex items-center justify-between p-4">
-                <button className="p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white" onClick={prev}>
+                <button className="opacity-30 text-blue-dark hover:text-red hover:opacity-50" onClick={prev}>
                     <ChevronLeft size={40}/>
                 </button>
-                <button className="p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white" onClick={next}>
+                <button className="opacity-30 text-blue-dark hover:text-red hover:opacity-50" onClick={next}>
                     <ChevronRight size={40}/>
                 </button>
             </div>
@@ -45,7 +51,7 @@ export function Carousel({ slides }: CarouselProps) {
             <div className="absolute inset-x-0 bottom-0 text-center">
                 {slides && slides.map((_, i) => (
                     <button key={i} 
-                        className={`w-2 h-2 mx-1 rounded-full bg-white/80 ${i == index ? "bg-white" : "bg-gray-800"}`} 
+                        className={`w-2 h-2 mx-1 rounded-full ${i == index ? "bg-red bg-opacity-50" : "bg-blue-dark bg-opacity-30"}`} 
                         onClick={() => setIndex(i)}/>
                 ))}
             </div>
