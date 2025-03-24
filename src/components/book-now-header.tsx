b13df947ede7f09e3548d55a534457599bb08d2a
Tim Heuett <tim.heuett@gmail.com>
2025-03-24 13:25:51 -0700

    remove mobile book now margin

--- src/components/book-now-header.tsx
+++ src/components/book-now-header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function BookNowHeader() {
   return (
     <div className="bg-black flex flex-row justify-around sticky top-0 z-50">
-      <div className="m-2 md:m-0 p-2 bg-red text-xl md:text-base">
+      <div className="p-2 bg-red text-xl md:text-base">
         <a className="text-white" href="/appointments">
           Book Now
         </a>
