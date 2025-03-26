5420cff4718e14b5cca323d546a50f813fc7d25f
Tim Heuett <tim.heuett@gmail.com>
2025-03-26 12:35:26 -0700

    simpler approach to square widget; resolves issue with navigating back and forth

--- src/components/square-appointments.tsx
+++ src/components/square-appointments.tsx
@@ -1,41 +1,14 @@
-"use client";
-
-import React, { useEffect, useRef } from "react";
-import Script from "next/script";
+import React from "react";
 
 export default function SquareAppointments() {
-  const containerRef = useRef(null);
-
-  // We have to manually move the script tag back into the container before it loads
-  // because Square's script appends the iFrame to the parent of the script
-  // and Next.js moves the script to the bottom of the body
-  useEffect(function () {
-    const squareWidget = document.querySelector(
-      "#square-appointments-widget-script",
-    );
-    containerRef.current.appendChild(squareWidget);
-  }, []);
-
-  // Similarly, style changes have to take place after the frame has been injected into the DOM
-  // by the Square script
-  const resizeFrame = () => {
-    const squareFrame = document.querySelector<HTMLElement>(
-      "#square-appointments-widget iframe",
-    );
-    squareFrame.style.height = "3600px";
-    squareFrame.style.outline = "none";
-  };
-
   return (
-    <div ref={containerRef} id="square-appointments-widget">
-      <Script
-        id="square-appointments-widget-script"
-        src={
-          "https://square.site/appointments/buyer/widget/55ac5595-a22b-46ef-bdea-3f552c1fd2c3/8X0P4KQ71MNP9.js"
-        }
-        onLoad={resizeFrame}
-        async
-      />
+    <div>
+      <iframe
+        src="https://book.squareup.com/appointments/55ac5595-a22b-46ef-bdea-3f552c1fd2c3/location/8X0P4KQ71MNP9/services"
+        className="w-full outline-0"
+        style={{ height: "3600px" }}
+        title="Sugar Peeps Appointment Booking"
+      ></iframe>
     </div>
   );
 }
