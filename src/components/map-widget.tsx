6fc26a5ae908aa16331f1912927c8287a843552e
Tim Heuett <tim.heuett@gmail.com>
2025-03-26 12:44:32 -0700

    use contact config for map widget

--- src/components/map-widget.tsx
+++ src/components/map-widget.tsx
@@ -1,10 +1,16 @@
+import { ContactAddress, ContactAddress2, ContactCity } from "@/constants";
 import React from "react";
 
 export default function MapWidget() {
+  const searchParam = encodeURIComponent(
+    `${ContactAddress}\n${ContactAddress2}\n${ContactCity}`,
+  );
+  const zoomLevel = 14;
+
   return (
     <iframe
       className="w-full h-96"
-      src={"https://www.google.com/maps?q=1417%20NW%2054th%20St%20-%20Studio%20432%0AInside%20StudioWorks%20Ballard%0ASeattle%20WA&output=embed&hl=en&z=14"}
+      src={`https://www.google.com/maps?q=${searchParam}&output=embed&hl=en&z=${zoomLevel}`}
     />
-  )
+  );
 }
