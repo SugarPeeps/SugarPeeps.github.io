f936a4eab076af418f9f7ed915ae3f21fe534073
Tim Heuett <tim.heuett@gmail.com>
2025-05-03 22:00:55 -0700

    update address to valhalla

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,7 +1,7 @@
 export const ContactEmail = "sugarpeepssugaring@gmail.com";
 export const ContactPhone = "(360) 421-8430";
-export const ContactAddress = "1417 NW 54th St - Studio 432";
-export const ContactAddress2 = "Inside StudioWorks Ballard";
+export const ContactAddress = "5306 Ballard Ave NW #211";
+export const ContactAddress2 = "Inside Valhalla Building, Floor 2";
 export const ContactCity = "Seattle, WA";
 export const InstagramUrl = "https://www.instagram.com/sugarpeepssugaring";
 export const FacebookUrl = "https://www.facebook.com/sugarpeepsbygen";
