43c9d372fb22a3668c59674d0e99b1d3fa1bdc28
Tim Heuett <tim.heuett@gmail.com>
2025-05-29 21:25:58 -0700

    extend height of booking widget frame

--- src/components/square-appointments.tsx
+++ src/components/square-appointments.tsx
@@ -6,7 +6,7 @@ export default function SquareAppointments() {
       <iframe
         src="https://book.squareup.com/appointments/55ac5595-a22b-46ef-bdea-3f552c1fd2c3/location/8X0P4KQ71MNP9/services"
         className="w-full outline-0"
-        style={{ height: "3600px" }}
+        style={{ height: "3800px" }}
         title="Sugar Peeps Appointment Booking"
       ></iframe>
     </div>
