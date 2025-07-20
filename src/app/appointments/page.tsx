fbf789fafa42f86f296f50547cfe8da82c89d4cb
Genevieve DuPuy <genevieve.e.dupuy@gmail.com>
2025-07-19 21:33:02 -0700

    took maintenance blurb off appt page

--- src/app/appointments/page.tsx
+++ src/app/appointments/page.tsx
@@ -1,23 +1,18 @@
-import React from "react";
-import { Metadata } from "next";
-import { Email } from "@/components/structure";
-import SquareAppointments from "@/components/square-appointments";
-import Link from "next/link";
+import React from 'react';
+import { Metadata } from 'next';
+import { Email } from '@/components/structure';
+import SquareAppointments from '@/components/square-appointments';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
-  title: "Appointments - Sugar Peeps",
+  title: 'Appointments - Sugar Peeps',
 };
 
 export default function Page() {
   return (
     <main className="bg-white" id="scheduling">
       <p>
-        Loyalty maintenance pricing discount of 15% will be applied at the time
-        of your service when your last sugar was received at Sugar Peeps 6 weeks
-        or less prior to your upkeep appointment. Huzzah for staying sweet!
-      </p>
-      <p>
-        If you have any questions or need help with scheduling, please{" "}
+        If you have any questions or need help with scheduling, please{' '}
         <Email>contact us</Email>.
       </p>
       <p>
